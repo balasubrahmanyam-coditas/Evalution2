cefbb0c81bc668c2323a79cd3b6bcf0934020a8d
bala subrahmanyam <bala.subryanam@coditas.com>
2023-02-24 17:01:11 +0530

    Evalution 4

---
+++ src/Evalution2/Evalution4.java
@@ -0,0 +1,52 @@
+//package Evalution4;
+//
+//import java.util.StringTokenizer;
+//
+//public class Evalution4 {
+//    public static void main(String[] args) {
+//        import java.util.Scanner;
+//import java.util.StringTokenizer;
+//import java.util.*;
+//        public class Question1 {
+//            public static void main(String[] args) {
+//                //Scanner sc = new Scanner(System.in);
+//                int vowel = 0;
+//                String sentence = "Hi,how are you?";
+//                String name = "Ramesh";
+//                String vowelstr = "";
+//                StringTokenizer stringTokenizer = new StringTokenizer(sentence);
+//                stringTokenizer.countTokens();
+//                char str[] = name.toCharArray();
+//                name.toLowerCase();
+//                for (int i = 0; i < name.length(); i++) {
+//                    if (name.charAt(i) == 'a' || name.charAt(i) == 'e' || name.charAt(i) == 'i' || name.charAt(i) == 'o' || name.charAt(i) == 'u') {
+//                        vowel++;
+//                        vowelstr += name.charAt(i);
+//                    }
+//                }
+//                String newstr="";
+//                for (int i = 0; i < name.length(); i++) {
+//                    if (name.charAt(i) == 'a' || name.charAt(i) == 'e' || name.charAt(i) == 'i' || name.charAt(i) == 'o' || name.charAt(i) == 'u') {
+//                        newstr += name.charAt(i);
+//                    }
+//                    else{
+//                        newstr+=(char)(name.charAt(i)+1);
+//                    }
+//                }
+//                System.out.println(vowelstr);
+//                System.out.println(newstr);
+//                System.out.println(stringTokenizer.countTokens());
+//            }
+//        }
+//
+///*
+//"C:\Program Files\Java\jdk1.8.0_131\bin\java.exe" "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.2\lib\idea_rt.jar=60183:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.2\bin" -Dfile.encoding=UTF-8 -classpath "C:\Program Files\Java\jdk1.8.0_131\jre\lib\charsets.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\deploy.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\ext\access-bridge-64.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\ext\cldrdata.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\ext\dnsns.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\ext\jaccess.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\ext\jfxrt.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\ext\localedata.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\ext\nashorn.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\ext\sunec.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\ext\sunjce_provider.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\ext\sunmscapi.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\ext\sunpkcs11.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\ext\zipfs.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\javaws.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\jce.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\jfr.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\jfxswt.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\jsse.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\management-agent.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\plugin.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\resources.jar;C:\Program Files\Java\jdk1.8.0_131\jre\lib\rt.jar;C:\Users\coditas\Evalution 4\out\production\Evalution 4" Question1
+//ae
+//Saneti
+//3
+//
+//Process finished with exit code 0
+//
+// */
+//    }
+//}
