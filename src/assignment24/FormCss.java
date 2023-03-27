33ea33cebffb3bfa8d922bd1346f9f1ddb61234e
bala subrahmanyam <bala.subryanam@coditas.com>
2023-03-27 15:36:37 +0530

    Assignment 23

---
+++ src/assignment24/FormCss.java
@@ -0,0 +1,50 @@
+package assignment24;
+
+public class FormCss {
+}
+/*
+#profile{
+    align-items: center;
+    text-align: center;
+}
+
+body{
+        background-image: url("");
+        background-color: #cccccc;
+}
+.container {
+    position: relative;
+    width: 100%;
+    height: 10%;
+  }
+  .circle {
+    position: absolute;
+    top: 2px;
+    right: 20px;
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    margin-top: 200 ;
+  }
+  .name {
+    position: relative;
+    top: 2px;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 3rem;
+    text-align: center;
+    text-decoration: underline;
+    background-color: aquamarine;
+  }
+  body{
+    border: 2px solid;
+  }
+
+  .profile img{
+    margin-top: 1000px;
+  }
+  th{
+    background-color:aqua;
+  }
+
+ */
---
+++ src/assignment24/FormHtml.java
@@ -0,0 +1,84 @@
+package assignment24;
+
+public class FormHtml {
+}
+/*
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="form.css" rel="stylesheet">
+</head>
+<body>
+    <div id="profile">
+        <h1 class="name">Bala Subrahmanyam</h1>
+        <img src="BB.jpeg" alt="Bala's Photo" height="200px" class="circle">
+    </div>
+    <br>
+    <br>
+    <div class ="container">
+        <img src="linkedIn.png" alt="linkedin" width="25x" height="25px" class="image">
+        <a href="https://www.linkedin.com/in/p-b-v-s-subramanyam-8a6a50233" target="_blank">linkdin Profile</a>
+    </div>
+    <br><br>
+    <fieldset>
+        <legend style="color:brown">Personal Details</legend>
+            <strong>Name:</strong>P.B.V.S.Subrahmanyam<br><br>
+            <strong>Address:</strong>Tadepalligudem,Andhra Pradesh<br><br>
+            <strong> Gender:</strong>Male<br><br>
+            <strong>Email:</strong>padalabalasubrhamanyam99@gmail.com<br><br>
+            <strong> DOB:</strong>10-08-2000<br><br>
+            <strong>PhoneNo:</strong>9966434499<br><br>
+    </fieldset>
+    <fieldset>
+        <legend style="color:brown">Educational Information</legend>
+        <head>
+            <style>table ,tr ,td {border: 20px solid white;}
+            </style>
+        </head>
+        <table>
+            <tr>
+                <th>Degree</th>
+                <th>College</th>
+                <th>Year Of Passing</th>
+                <th>Percentage</th>
+                <th> Pass/Fail</th>
+            </tr>
+            <tr>
+                <td>B.E</td>
+                <td>Sathyabama University</td>
+                <td>2023</td>
+                <td>88</td>
+                <td>Pass</td>
+            </tr>
+            <tr>
+                <td>Intermedite</td>
+                <td>Sasi New.Gen college</td>
+                <td>2019</td>
+                <td>92</td>
+                <td>Pass</td>
+            </tr>
+            <tr>
+                <td>School</td>
+                <td>Sasi Highschool</td>
+                <td>2017</td>
+                <td>92</td>
+                <td>Pass</td>
+            </tr>
+        </table>
+    </fieldset>
+    <fieldset>
+        <legend  style="color:brown">About </legend>
+        <strong>Hobbiess:</strong>Cricket<br><br>
+        <strong>Achievements:</strong>CSS<br><br>
+        <strong>KeyPoints:</strong>Learner<br><br>
+        <strong>Skills:</strong>Java<br><br>
+    </fieldset>
+
+</body>
+</html>
+ */
